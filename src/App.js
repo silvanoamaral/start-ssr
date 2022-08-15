ff73f33ff03ee00542db0f1c79d3e7b70163674f
silvanoamaral <silvano.amaral9@gmail.com>
2022-08-15 14:13:37 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function ComponenteTeste({ texto }) {
+  return (
+    <p>
+      Carregando o Aplicativo no <strong>{texto}</strong>
+    </p>
+  );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <ComponenteTeste texto="server" />;
 }
 
 export default App;
