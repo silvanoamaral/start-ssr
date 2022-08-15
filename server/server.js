c81d2f221c986c5598d3c2bc2ecb97b7c26e0835
silvanoamaral <silvano.amaral9@gmail.com>
2022-08-15 14:43:01 -0300

    add route

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const PORT = 3000;
 
 const app = express();
 
-app.use("^/$", (req, res, next) => {
+app.use("/server-client", (req, res, next) => {
   fs.readFile(path.resolve("./build/index.html"), "utf-8", (err, data) => {
     if (err) {
       console.log(err);
